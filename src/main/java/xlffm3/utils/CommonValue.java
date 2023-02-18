fdb42b1092ea3533a7dfe248db3e9d6a47b15891
minhee.son <minhee4735@gmail.com>
2023-02-18 14:15:02 +0900

    [xlffm3] stopped

--- src/main/java/xlffm3/utils/CommonValue.java
+++
@@ -1,9 +0,0 @@
-package xlffm3.utils;
-
-public class CommonValue {
-
-    public static final int ZERO = 0;
-
-    public static final String COMMA = ",";
-
-}
--- src/main/java/xlffm3/utils/ErrorMessage.java
+++
@@ -1,9 +0,0 @@
-package xlffm3.utils;
-
-public class ErrorMessage {
-
-    public static final String OVER_BUDGET = "구입 금액을 초과하는 개수입니다.";
-    public static final String NOT_ENOUGH_MANUAL_COUNT = "수동 로또는 최소 1개이상 구매해야합니다.";
-    public static final String NOT_ENOUGH_BUDGET = "로또의 최소 구매금액은 1000원 이상이어야 합니다.";
-    public static final String INVALID_MONEY = "구입금액은 1000원 단위로 입력해주세요.";
-}
